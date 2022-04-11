f4b160c7f31ada677ccfea7a3747a94a0eacdba1
Csummors <60412568+Csummors@users.noreply.github.com>
2022-04-11 10:35:06 -0400

    update the pattern and button

--- SS7_AllOfTheLights/SS7_AllOfTheLights.ino
+++ SS7_AllOfTheLights/SS7_AllOfTheLights.ino
@@ -29,23 +29,22 @@ void loop() {
 
   if(buttonState==HIGH){
     //pattern 01
-   digitalWrite(LED4,HIGH);digitalWrite(LED2,HIGH);digitalWrite(LED1,HIGH);
-   delay (300);
-   digitalWrite(LED4,LOW);  digitalWrite(LED2,LOW); digitalWrite(LED1,LOW);
-   delay (300);
-   digitalWrite(LED0,HIGH);digitalWrite(LED3,HIGH);digitalWrite(LED5,HIGH);
-   delay (300); 
-   digitalWrite(LED0,LOW);digitalWrite(LED3,LOW); digitalWrite(LED5,LOW); 
-   delay (300);
-    }else{
-      //pattern 02
+    //Chage the pattern to one 
       digitalWrite (LED4,HIGH); digitalWrite (LED5, HIGH);
       delay (80);
       digitalWrite (LED0,HIGH); digitalWrite (LED3, HIGH);
       delay (80);
       digitalWrite (LED1,HIGH); digitalWrite (LED2, HIGH);
+      digitalWrite (LED4,LOW); digitalWrite (LED5, LOW);
+      delay (80);
+      digitalWrite (LED0,LOW); digitalWrite (LED3, LOW);
+      delay (80);
+      digitalWrite (LED1,LOW); digitalWrite (LED2, LOW);
 
-       digitalWrite (LED4,LOW); digitalWrite (LED5, LOW);
+    }else{
+      //now lights turn off when LOW
+      
+      digitalWrite (LED4,LOW); digitalWrite (LED5, LOW);
       delay (80);
       digitalWrite (LED0,LOW); digitalWrite (LED3, LOW);
       delay (80);
